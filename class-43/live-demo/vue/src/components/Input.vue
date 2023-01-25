dfe53418a8ae8c94d447f06247c87aa6ecd04b5d
Ryan Gallaway <rkgallaway@msn.com>
2023-01-25 12:18:43 -0800

    assets added

---
+++ class-43/live-demo/vue/src/components/Input.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+      avg: '',
+      min: '',
+      max: '',
+    }
+  }
+}
+
+</script>
+
+<template>
+  <p>The Store Name is {{ name }}, avg is {{ avg  }}, min is {{ min }}, max is{{ max }}</p>
+  <label>Enter Name:
+    <input v-model="name" placeholder="Store Name" />
+  </label>
+  <label>Enter avg:
+    <input v-model="avg" placeholder="avg" />
+  </label>
+  <label>Enter min:
+    <input v-model="min" placeholder="min" />
+  </label>
+  <label>Enter max:
+    <input v-model="max" placeholder="max" />
+  </label>
+</template>
